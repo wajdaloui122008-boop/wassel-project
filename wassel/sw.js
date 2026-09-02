85b502e4d8d09af846132858b056ebcb83c3f637
wajdaloui122008-boop <wajd.aloui12.2008@gmail.com>
2026-09-02 19:22:31 +0100

    fix: invalidate PWA shell after service catalog release

--- wassel/sw.js
+++ wassel/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "velto-shell-v4";
+const CACHE = "velto-shell-v5";
 const SHELL = [
   "/", "/index.html", "/style.css", "/script.js", "/tracking.js",
   "/dashboard2.js", "/driver-map.js", "/services.js", "/client-enhancements.js",
