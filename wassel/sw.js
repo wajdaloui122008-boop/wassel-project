2408c657598d1b46812fd6d1ed79376290c125ca
wajdaloui122008-boop <wajd.aloui12.2008@gmail.com>
2026-09-02 21:20:35 +0100

    fix: refresh PWA shell after live tracking updates

--- wassel/sw.js
+++ wassel/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "velto-shell-v6";
+const CACHE = "velto-shell-v7";
 const ASSETS = [
   "/",
   "/index.html",
@@ -29,8 +29,10 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    if (response.ok) {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    }
     return response;
   }).catch(() => caches.match(event.request)));
 });
